7b6687dde8cdf88c21e288e01dd5c2a57939c4b8
gamboabill <peterbillgamboa@gmail.com>
2025-03-09 12:20:19 +0800

    updated

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -32,7 +32,8 @@
                 <strong>Tags:</strong> {{ ucwords(str_replace(',', ', ', $job->tags)) }}
             </li>
         @endif
-        {{ $job->user->name }}
+        <li class="mb-2"><strong>Posted By: </strong>{{ $job->user->name }}</li>
+
     </ul>
     <a href="{{ route('jobs.show', $job->id) }}"
         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
